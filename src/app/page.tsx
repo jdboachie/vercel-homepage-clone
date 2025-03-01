59191afdb5e6b1cc118b1ea06a4dedf68f89df43
Jude Boachie <jdboachie@gmail.com>
2025-03-01 00:04:58 +0000

    hotupdates .

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -34,7 +34,7 @@ const Conformance = () => {
         </p>
       </div>
       <div className="border rounded-t-lg">
-        
+
       </div>
     </div>
   )
@@ -52,7 +52,7 @@ const InstantRollbacks = () => {
           <span className="text-primary font-semibold ">Go ahead, deploy on Friday.</span> Instantly rollback to a working deployment.
         </p>
         <div className="grid mt-4 text-sm">
-          <div className="border items-center max-w-[330px] w-fit flex shadow-sm bg-white rounded-lg p-3 gap-4">
+          <div className="border items-center max-w-[330px] w-fit flex shadow-sm bg-background rounded-lg p-3 gap-4">
             <div className="grid grow gap-1">
               <div className="flex justify-between text-muted-foreground"><p className="max-lg:hidden">vercel-site/<span className="text-primary">jvjb4ynna</span></p><p className="max-lg:text-xs">1d ago</p></div>
               <p className="flex justify-between items-center gap-4"><span className="text-xs items-end h-full flex">ba5f55f</span><span>Update bento<span className="max-lg:hidden"> box design</span></span></p>
@@ -99,13 +99,13 @@ const InstantRollbacks = () => {
                 <clipPath id="clip0_6_107"><rect fill="white" height="8" width="8"></rect></clipPath>
               </defs>
             </svg>
-            <div className="absolute left-[calc(50%-8px)] top-[calc(50%-20px)] border bg-white p-2 rounded-full" data-version="v1">
+            <div className="absolute left-[calc(50%-8px)] top-[calc(50%-20px)] border bg-background p-2 rounded-full" data-version="v1">
               <svg data-testid="geist-icon" height="16" stroke-linejoin="round" viewBox="0 0 16 16" width="16">
                 <path fillRule="evenodd" clipRule="evenodd" d="M7.96452 2.5C11.0257 2.5 13.5 4.96643 13.5 8C13.5 11.0336 11.0257 13.5 7.96452 13.5C6.12055 13.5 4.48831 12.6051 3.48161 11.2273L3.03915 10.6217L1.828 11.5066L2.27046 12.1122C3.54872 13.8617 5.62368 15 7.96452 15C11.8461 15 15 11.87 15 8C15 4.13001 11.8461 1 7.96452 1C5.06835 1 2.57851 2.74164 1.5 5.23347V3.75V3H0V3.75V7.25C0 7.66421 0.335786 8 0.75 8H3.75H4.5V6.5H3.75H2.63724C3.29365 4.19393 5.42843 2.5 7.96452 2.5ZM8.75 5.25V4.5H7.25V5.25V7.8662C7.25 8.20056 7.4171 8.51279 7.6953 8.69825L9.08397 9.62404L9.70801 10.0401L10.5401 8.79199L9.91603 8.37596L8.75 7.59861V5.25Z" fill="currentColor"></path>
               </svg>
             </div>
           </div>
-          <div className="justify-self-end border border-dashed items-center max-w-[330px] w-fit flex shadow-sm bg-white rounded-lg p-3 gap-4">
+          <div className="justify-self-end border border-dashed items-center max-w-[330px] w-fit flex shadow-sm bg-background rounded-lg p-3 gap-4">
             <div className="grid grow gap-1">
               <div className="flex justify-between text-muted-foreground"><p className="max-lg:hidden">vercel-site/<span className="text-primary">gigj178pv</span></p><p className="max-lg:text-xs">10m ago</p></div>
               <p className="flex justify-between items-center gap-4"><span className="text-xs items-end h-full flex">bx012mm</span><span>Fix ESLint error<span className="max-lg:hidden"> on query</span></span></p>
